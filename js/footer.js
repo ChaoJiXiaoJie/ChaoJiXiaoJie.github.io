4823ec9ebaae749fdd4c0f6520d26356077cc788
Administrator <0xf00000000@gmail.com>
2017-02-16 17:16:18 +0800

    change test！

--- js/footer.js
+++ js/footer.js
@@ -3,18 +3,27 @@
  */
 $(document).ready(function(){
     $(".qq").click(function(){
-        if(!$("#showImgP")[0]){
-            var height = ($(window).height()-518)/2;
-            var width = ($(window).width()-378)/2;
-            // alert(height);
-            // alert(width);
-            var boarddiv = "<div id='showImgP' style='background:rgba(196,18,54,0.4);width:100%;height:100%;z-index:998;position:fixed;top:0;'><img id='showImg' src='img/qqlink.jpg' style='width: 378px;height: 518px;position: fixed;padding: "+height+"px "+width+"px;'></div>";
-            $(document.body).append(boarddiv);
-            $("#showImgP").click(function(){
-                $("#showImgP").hide();
-            });
-        }else{
-            $("#showImgP").show();
-        };
+        if($("#showImgP")[0]){
+            $("#showImgP").remove();
+        }
+        var height = ($(window).height()-518)/2>0?($(window).height()-518)/2:0;
+        var width = ($(window).width()-378)/2>0?($(window).width()-378)/2:0;
+        var boarddiv = "<div id='showImgP' style='background:rgba(196,18,54,0.4);width:100%;height:100%;z-index:999;position:fixed;top:0;'><img id='showImg' src='img/qqlink.jpg' style='width: 378px;height: 518px;position: fixed;padding: "+height+"px "+width+"px;'></div>";
+        $(document.body).append(boarddiv);
+        $("#showImgP").click(function(){
+            $("#showImgP").hide();
+        });
+    });
+    $(".weChat").click(function(){
+        if($("#showImgPwe")[0]){
+            $("#showImgPwe").remove();
+        }
+        var height = ($(window).height()-512)/2>0?($(window).height()-512)/2:0;
+        var width = ($(window).width()-512)/2>0?($(window).width()-512)/2:0;
+        var boarddiv = "<div id='showImgPwe' style='background:rgba(196,18,54,0.4);width:100%;height:100%;z-index:998;position:fixed;top:0;'><img id='showImgwe' src='img/WeChatLink.png' style='width: 512px;height: 512px;position: fixed;padding: "+height+"px "+width+"px;'></div>";
+        $(document.body).append(boarddiv);
+        $("#showImgPwe").click(function(){
+            $("#showImgPwe").hide();
+        });
     });
 });
